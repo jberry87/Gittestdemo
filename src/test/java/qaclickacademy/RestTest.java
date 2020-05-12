da077bb2c807640a547e4b421fa7df75bc9134a6
jyotika <berryjyotika37@gmail.com>
2020-05-12 14:18:05 -0400

    firstcommit from cdrive

---
+++ src/test/java/qaclickacademy/RestTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestTest {
+	
+	@Test
+	public void browsetest3()
+	{
+		System.out.println("Test with maven in selenium3");
+	}
+	
+   @Test
+	public void browsetest4()
+	{
+		System.out.println("Test with maven in selenium4");
+	}
+}
+
+
+
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+		
+		@Test
+		public void browsetest()
+		{
+			System.out.println("Test with maven in selenium");
+		}
+		
+
+		@Test
+		public void browsetest2()
+		{
+			System.out.println("Test with maven in selenium2");
+		}
+	}
+
+
